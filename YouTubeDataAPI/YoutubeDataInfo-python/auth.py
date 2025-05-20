013c64168f770a8ffce56291e3b5ba8c7feb93fd
vibhmitra <66880260+vibhmitra@users.noreply.github.com>
2025-05-20 12:17:02 +0000

    🔐 authorization

---
+++ YouTubeDataAPI/YoutubeDataInfo-python/auth.py
@@ -0,0 +1,25 @@
+# Import necessary libraries
+import os
+import json
+from dotenv import dotenv_values
+
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+# LOAD ENVIRONMENT VARIABLES
+ENV = dotenv_values('.env')
+
+# Set up the path to the client secrets file
+CLIENT_SECRETS_FILE = os.path.join(os.path.dirname(__file__), ENV['CLIENT_SECRETS_JSON'])
+SCOPES = ['https://www.googleapis.com/auth/youtube.readonly']
+
+def get_authenticated_service():
+    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+    auth_url, _ = flow.authorization_url()
+    credentials = flow.run_local_server(open_browser=False)
+    print(credentials)
+    return credentials
+
+
+get_authenticated_service()
+
+
